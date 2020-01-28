3ab5211cc4b977cd6a1015047bf151fa2268b434
muchafilip7@gmail.com <muchafilip7@gmail.com>
2020-01-28 17:59:51 +0100

    28.01.2020

--- src/test/java/pl/fyv/flightmanager/BookingModelClassTest.java
+++ src/test/java/pl/fyv/flightmanager/BookingModelClassTest.java
@@ -1,5 +1,6 @@
 package pl.fyv.flightmanager;
 
+import org.joda.time.LocalDateTime;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import pl.fyv.flightmanager.model.Booking;
@@ -8,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class BookingModelClassTest {
     public static Booking booking;
+    private LocalDateTime arrive = LocalDateTime.parse("2020-01-03T12:34:00");
 
     @BeforeClass
     public static void setUp() {
@@ -18,7 +20,26 @@ public class BookingModelClassTest {
     public void isSetItemId() {
         booking.setItemId(22);
         assertEquals(22, booking.getItemId());
-
+    }
+    @Test
+    public void isSetClientId() {
+        booking.setClientId(8);
+        assertEquals(8, booking.getClientId());
+    }
+    @Test
+    public void isSetStatus() {
+        booking.setStatus("on");
+        assertEquals("on", booking.getStatus());
+    }
+    @Test
+    public void isSetArrive() {
+        booking.setArrive(java.time.LocalDateTime.parse("2020-01-02T11:33:55"));
+        assertEquals("2020-01-02T11:33:55", booking.getArrive());
+    }
+    @Test
+    public void isSetDeparture() {
+        booking.setDeparture(java.time.LocalDateTime.parse("2020-01-03T12:34:00"));
+        assertEquals("2020-01-02T11:33:55", booking.getDeparture());
     }
     }
 
--- src/test/java/pl/fyv/flightmanager/FlightModelTest.java
+++ src/test/java/pl/fyv/flightmanager/FlightModelTest.java
@@ -1,13 +1,16 @@
 package pl.fyv.flightmanager;
 
+import org.joda.time.LocalDateTime;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import pl.fyv.flightmanager.model.Flight;
 
 import static org.junit.Assert.assertEquals;
+import static pl.fyv.flightmanager.BookingModelClassTest.booking;
 
 public class FlightModelTest {
     public static Flight flight;
+    private LocalDateTime arrive = LocalDateTime.parse("2020-01-03T12:34:00");
 
     @BeforeClass
     public static void setUp() {
@@ -15,10 +18,39 @@ public class FlightModelTest {
     }
 
     @Test
-
-        public void isSetDeparturePlace() {
-            flight.setDeparturePlace("London");
+    public void isSetDeparturePlace() {
+        flight.setDeparturePlace("London");
         assertEquals("London", flight.getDeparturePlace());
     }
+
+    @Test
+    public void isSetArrivalPlace() {
+        flight.setArrivalPlace("Madrid");
+        assertEquals("Madrid", flight.getArrivalPlace());
     }
+
+    @Test
+    public void isSetArrivalTime() {
+        booking.setArrive(java.time.LocalDateTime.parse("2020-01-02T11:33:55"));
+        assertEquals("2020-01-02T11:33:55", booking.getArrive());
+    }
+
+    @Test
+    public void isSetDepartureTime() {
+        booking.setDeparture(java.time.LocalDateTime.parse("2020-01-03T12:34:00"));
+        assertEquals("2020-01-02T11:33:55", booking.getDeparture());
+    }
+
+    @Test
+    public void isFreeSeats() {
+        flight.setArrivalPlace("58B");
+        assertEquals("58B", flight.getFreeSeats());
+    }
+
+  /*  @Test
+    public void isPrice() {
+        flight.setPrice(139.99);
+        assertEquals(139.99, flight.getPrice());
+    }  */
+}
 
