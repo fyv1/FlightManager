694a0504070523e2bde5e3e249bf0ec58db53538
fyv1 <grzegorz.nowak.formal@gmail.com>
2020-01-28 16:54:36 +0100

    First API test

--- src/test/java/pl/fyv/flightmanager/FlightmanagerApplicationTests.java
+++ src/test/java/pl/fyv/flightmanager/FlightmanagerApplicationTests.java
@@ -1,37 +1,5 @@
 package pl.fyv.flightmanager;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-import pl.fyv.flightmanager.controller.CarController;
-import pl.fyv.flightmanager.model.Car;
-import pl.fyv.flightmanager.repository.CarRepo;
-
-import static org.junit.Assert.*;
-
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
 public class FlightmanagerApplicationTests {
-
-    @Mock
-    Car car;
-    @Mock
-    CarController controller;
-
-//    @Test
-//    public void contextLoads() {
-//    }
-
-    @Test
-    public void testMockito() {
-
-
-
-    }
-
 
 }
